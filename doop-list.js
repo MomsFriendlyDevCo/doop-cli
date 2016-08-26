12f89e177aeb7cbc56c8a2f087a93eac06e4317d
Matt Carter <m@ttcarter.com>
2016-08-26 11:40:07 +1000

    List command now working

---
+++ doop-list.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var async = require('async-chainable');
+var colors = require('chalk');
+var doop = require('.');
+var program = require('commander');
+
+program
+	.version(require('./package.json').version)
+	.description('List units installed for the current project')
+	.option('-s, --server', 'Limit list to only server units (cannot be used with `--client`)')
+	.option('-c, --client', 'Limit list to only client units (cannot be used with `--server`)')
+	.option('-v, --verbose', 'Be verbose. Specify multiple times for increasing verbosity', function(i, v) { return v + 1 }, 0)
+	.parse(process.argv);
+
+async()
+	.then(doop.chProjectRoot)
+	.parallel({
+		client: function(next) {
+			if (program.server) return next();
+			doop.getUnits(next, 'client');
+		},
+		server: function(next) {
+			if (program.server) return next();
+			doop.getUnits(next, 'server');
+		},
+	})
+	.then(function(next) {
+		[
+			!program.server ? 'client' : null,
+			!program.client ? 'server' : null,
+		]
+			.filter(i => !!i) // Filter out empty
+			.forEach(type => {
+				console.log(colors.bold.blue(type));
+				this[type].forEach(unit => console.log(' -', unit));
+			});
+		next();
+	})
+	.end(function(err) {
+		if (err) {
+			console.log(colors.red('Doop Error'), err.toString());
+			process.exit(1);
+		} else {
+			process.exit(0);
+		}
+	});

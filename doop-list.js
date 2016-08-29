3d2b59b33bbd81e6c422134b97e463bd20f11fbe
Matt Carter <m@ttcarter.com>
2016-08-29 11:32:49 +1000

    List command can now compare file hashes

--- doop-list.js
+++ doop-list.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
+var _ = require('lodash');
 var async = require('async-chainable');
 var colors = require('chalk');
 var doop = require('.');
+var glob = require('glob');
+var fs = require('fs');
+var fspath = require('path');
 var program = require('commander');
+var sha1 = require('node-sha1');
 
 program
 	.version(require('./package.json').version)
 	.description('List units installed for the current project')
+	.option('-b, --basic', 'Display a simple list, do not attempt to hash file differences')
 	.option('-s, --server', 'Limit list to only server units (cannot be used with `--client`)')
 	.option('-c, --client', 'Limit list to only client units (cannot be used with `--server`)')
 	.option('-v, --verbose', 'Be verbose. Specify multiple times for increasing verbosity', function(i, v) { return v + 1 }, 0)
@@ -20,16 +26,95 @@ async()
 	.parallel({
 		client: function(next) {
 			if (program.server) return next();
-			doop.getUnits(next, 'client');
+			doop.getUnits(function(err, units) {
+				if (err) return next(err);
+				next(null, units.map(u => { return {id: u, path: fspath.join(doop.settings.paths.client, u), files:{}} }));
+			}, 'client');
 		},
 		server: function(next) {
 			if (program.server) return next();
-			doop.getUnits(next, 'server');
+			doop.getUnits(function(err, units) {
+				if (err) return next(err);
+				next(null, units.map(u => { return {id: u, path: fspath.join(doop.settings.paths.server, u), files:{}} }));
+			}, 'server');
 		},
 	})
 	// }}}
+	// Hash file comparisons unless program.basic {{{
+	// Get repo {{{
+	.then('repo', function(next) {
+		if (program.basic) return next();
+		doop.getDoopPath(next, program.repo);
+	})
+	.then(function(next) {
+		if (program.basic) return next();
+		if (program.verbose) console.log('Using Doop source:', colors.cyan(this.repo));
+		next();
+	})
+	// }}}
+	// Scan project + Doop file list and hash all files (unless !program.basic) {{{
+	.then(function(next) {
+		if (program.basic) return next();
+
+		// Make a list of all files in both this project and in the doop repo
+		// For each file create an object with a `local` sha1 hash and `doop` sha1 hash
+
+		var hashQueue = async(); // Hash tasks to perform
+
+		async()
+			.forEach(_.concat(this.client, this.server), function(next, unit) {
+				async()
+					.parallel([
+						function(next) {
+							glob(fspath.join(unit.path, '**'), {nodir: true}, function(err, files) {
+								if (_.isArray(files))
+									files.forEach(function(file) {
+										hashQueue.defer(file, function(next) {
+											if (program.verbose) console.log('Hash file (Proj)', colors.cyan(file));
+											sha1(fs.createReadStream(file), function(err, hash) {
+												if (!unit.files[file]) unit.files[file] = {path: file};
+												unit.files[file].project = hash;
+												next();
+											});
+										});
+									});
+								next();
+							});
+						},
+						function(next) {
+							glob(fspath.join(doop.settings.paths.doop, unit.path, '**'), {nodir: true}, function(err, files) {
+								if (_.isArray(files))
+									files.forEach(function(rawFile) {
+										var croppedPath = rawFile.substr(doop.settings.paths.doop.length + 1);
+										var file = fspath.join(doop.settings.paths.doop, croppedPath);
+
+										hashQueue.defer(file, function(next) {
+											if (program.verbose) console.log('Hash file (Doop)', colors.cyan(croppedPath));
+											sha1(fs.createReadStream(file), function(err, hash) {
+												if (!unit.files[croppedPath]) unit.files[croppedPath] = {path: file};
+												unit.files[croppedPath].doop = hash;
+												next();
+											});
+										});
+									});
+								next();
+							});
+						},
+					])
+					.end(next)
+			})
+			.end(function(err) {
+				if (err) return next(err);
+
+				// Wait for hashing queue to finish
+				hashQueue.await().end(next);
+			});
+	})
+	// }}}
+	// }}}
 	// Present the list {{{
 	.then(function(next) {
+		if (program.verbose > 1) console.log();
 		[
 			!program.server ? 'client' : null,
 			!program.client ? 'server' : null,
@@ -37,7 +122,29 @@ async()
 			.filter(i => !!i) // Filter out empty
 			.forEach(type => {
 				console.log(colors.bold.blue(type));
-				this[type].forEach(unit => console.log(' -', unit));
+				if (program.basic) { // Dont bother to do any weird hash comparison operations
+					this[type].forEach(unit => console.log(' -', unit.id));
+				} else {
+					this[type].forEach(function(unit) {
+						var changes = [];
+						// Edited:
+						_.filter(unit.files, f => f.project && f.doop && f.project != f.doop)
+							.forEach(f => changes.push(colors.yellow.bold('~') + f.path.substr(unit.path.length+1)));
+						// Created:
+						_.filter(unit.files, f => f.project && !f.doop)
+							.forEach(f => changes.push(colors.green.bold('+') + f.path.substr(unit.path.length+1)));
+						// Deleted
+						_.filter(unit.files, f => f.doop && !f.project)
+							.forEach(f => changes.push(colors.red.bold('-') + f.path.substr(doop.settings.paths.doop.length+unit.path.length+2)));
+
+
+						if (changes.length) {
+							console.log(' -', unit.id, colors.blue('('), changes.join(', '), colors.blue(')'));
+						} else {
+							console.log(' -', unit.id);
+						}
+					});
+				}
 			});
 		next();
 	})

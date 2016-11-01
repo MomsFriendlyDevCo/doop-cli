df661cacf2c2a98db76b2f8f32c161f471931447
Matt Carter <m@ttcarter.com>
2016-11-01 11:37:28 +1000

    Doop-unknown helper can now carry over argv arguments into the command it runs

--- doop-unknown.js
+++ doop-unknown.js
@@ -9,10 +9,12 @@
 var _ = require('lodash');
 var async = require('async-chainable');
 var asyncFlush = require('async-chainable-flush');
+var childProcess = require('child_process');
 var colors = require('chalk');
 var doop = require('.');
 var fspath = require('path');
 var glob = require('glob');
+var spawnArgs = require('spawn-args');
 
 var cmd = process.argv[2]; // Exclude node + filename
 
@@ -31,21 +33,33 @@ async()
 	.then(function(next) {
 		if (!cmd && doop.settings.aliases.default) { // No command given (and we support a default command) - substitute `doop.settings.aliases.default`
 			cmd = doop.settings.aliases.default;
-			require('./doop-' + cmd);
 		} else if (doop.settings.aliases[cmd]) { // Command is an alias - redirect to that
 			cmd = doop.settings.aliases[cmd];
-			require('./doop-' + cmd);
+			cmd += ' ' + process.argv // Append any remaining ARGV stuff
+				.slice(3)
+				.join(' ');
+			cmd = _.trim(cmd);
 		} else if (_.includes(this.commands, cmd)) { // Existing command given - will be handled by commanders upstream require()
-			return next('SKIP');
+			return next('STOP');
 		}
+
+		// If we got here cmd should be populated
+		if (!cmd.startsWith('!')) cmd = doop.settings.paths.doopCli + ' ' + cmd; // If command doesn't begin with '!' assume it should have a doop prefix ('!' prefixing copied shamelessly from Git)
+
+		var cmdArgs = spawnArgs(cmd);
+		var mainCmd = cmdArgs.shift();
+
+		var editor = childProcess.spawn(mainCmd, cmdArgs, {stdio: 'inherit'});
+
+		editor.on('error', next);
+		editor.on('close', code => next(code != 0 ? 'Exited with code ' + code : 'STOP'));
 	})
 	// }}}
 	// End {{{
 	.flush()
 	.end(function(err) {
-		console.log(this.commands);
-		if (err && err == 'SKIP') { // Die quietly - usually because some other process is handling the end condition
-			console.log('DIE QUIET!');
+		if (err && err == 'STOP') { // Die quietly - usually because some other process is handling the end condition
+			process.exit(0);
 		} else if (err) {
 			console.log(colors.red('Doop Error'), err.toString());
 			process.exit(1);
